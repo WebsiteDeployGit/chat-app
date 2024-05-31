8e3f669c88956f917b4ab81d130d98159da00b60
iamsmita <95167216+iamsmita@users.noreply.github.com>
2024-06-01 01:07:12 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-
+import path from "path"
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -10,7 +10,7 @@ import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app,server } from "./socket/socket.js";
 
 const PORT =process.env.PORT || 5000;
-
+const __dirname =path.resolve();
 dotenv.config();
 
 app.use(express.json())// to parse the incoming requests with JSON payloads (from req.body)
@@ -20,6 +20,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT,()=>{  
     connectToMongoDB();
