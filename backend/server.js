3364bd8e41d15d8a754589c0c800cd04f4328961
iamsmita <95167216+iamsmita@users.noreply.github.com>
2024-06-01 00:53:29 +0530

    completed socket

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,8 @@ import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app,server } from "./socket/socket.js";
 
-const app=express();
 const PORT =process.env.PORT || 5000;
 
 dotenv.config();
@@ -21,6 +21,6 @@ app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
 
-app.listen(PORT,()=>{  
+server.listen(PORT,()=>{  
     connectToMongoDB();
     console.log(`Server running on ${PORT}`)})
