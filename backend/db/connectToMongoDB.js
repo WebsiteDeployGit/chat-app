f9846039b175ccd70c63c8b902cee3544b564f1a
iamsmita <95167216+iamsmita@users.noreply.github.com>
2024-06-01 01:51:17 +0530

    6th deployment

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -1,12 +1,20 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const connectToMongoDB = async () => {
-	try {
-		await mongoose.connect(process.env.MONGO_DB_URI);
-		console.log("Connected to MongoDB");
-	} catch (error) {
-		console.log("Error connecting to MongoDB", error.message);
-	}
+  const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 50000, // Increase timeout to 50 seconds
+    socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+    connectTimeoutMS: 30000, // Increase connection timeout to 30 seconds
+  };
+
+  try {
+    await mongoose.connect(process.env.MONGO_DB_URI, options);
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.log('Error connecting to MongoDB', error.message);
+  }
 };
 
 export default connectToMongoDB;
